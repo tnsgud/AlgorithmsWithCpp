67f06462301dc0da146f91dd263810427f5921a9
tnsgud <qkrtnsgud0229@gmail.com>
2022-01-11 22:07:40 +0900

    [ADD] 5339문제 풀이

---
+++ src/implementation/5339.cpp
@@ -0,0 +1,25 @@
+//
+// Created by qkrtn on 2022-01-11.
+// Link : https://www.acmicpc.net/problem/5339
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout <<"     /~\\" << '\n';
+    cout <<"    ( oo|" << '\n';
+    cout <<"    _\\=/_" << '\n';
+    cout <<"   /  _  \\" << '\n';
+    cout <<R"(  //|/.\|\\)" << '\n';
+    cout <<" ||  \\ /  ||" << '\n';
+    cout <<"============" << '\n';
+    cout <<"|          |" << '\n';
+    cout <<"|          |" << '\n';
+    cout <<"|          |";
+
+    return 0;
+}
