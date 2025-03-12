d6c920b2a42de741bfff9d347396a694a9c3a2f3
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-12 13:53:09 +0000

    🚩 Add the solution file for programmers No. 120803

---
+++ src/programmers/level_0/120803.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 - num2;
+
+    return answer;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr); 
+
+    return 0;
+}
