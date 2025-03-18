f74e01e34d2d1817549da6b2ad1dd37133a673f7
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-18 11:06:43 +0000

    🚩 Add the solution file for programmers No.181947

---
+++ src/programmers/level_0/181947.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int a, b;
+
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+
+    return 0;
+}
