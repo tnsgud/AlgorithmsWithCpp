f4ecc43938dab6ab3d03d2b02a14992745fafda1
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-19 11:22:42 +0000

    💊 하루한알 완료

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+
+    for(int i = my_string.size() - 1; 0 <= i; i--) {
+        answer += my_string[i];
+    }
+
+    return answer;
+}
 
 int main() {
     ios::sync_with_stdio(false);
