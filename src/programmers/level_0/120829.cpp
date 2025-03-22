8a2b8b685143fc2c74e46d51514be59a0665e951
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-22 06:12:34 +0000

    🚩 Add the solution file for programmers No.120829

---
+++ src/programmers/level_0/120829.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(int angle) {
+    int answer = 0;
+
+    if(0 < angle && angle < 90) answer = 1;
+    else if(angle == 90) answer = 2;
+    else if(90 < angle && angle < 180) answer = 3;
+    else if(angle == 180) answer = 4;
+
+    return answer;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    return 0;
+}
