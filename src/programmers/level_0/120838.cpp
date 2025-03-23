89dd46e0bdefc7978ac9dd47cb227e5ddde60e83
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-23 06:12:43 +0000

    🚩 Add the solution file for programmers No.120838

---
+++ src/programmers/level_0/120838.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+map<string, string> morse = {
+    {".-", "a"},
+    {"-...", "b"},
+    {"-.-.", "c"},
+    {"-..", "d"},
+    {".", "e"},
+    {"..-.", "f"},
+    {"--.", "g"},
+    {"....", "h"},
+    {"..", "i"},
+    {".---", "j"},
+    {"-.-", "k"},
+    {".-..", "l"},
+    {"--", "m"},
+    {"-.", "n"},
+    {"---", "o"},
+    {".--.", "p"},
+    {"--.-", "q"},
+    {".-.", "r"},
+    {"...", "s"},
+    {"-", "t"},
+    {"..-", "u"},
+    {"...-", "v"},
+    {".--", "w"},
+    {"-..-", "x"},
+    {"-.--", "y"},
+    {"--..", "z"},
+};
+
+string solution(string letter)
+{
+  string answer = "";
+  int startIndex = 0, endIndex = 0;
+
+  while (true)
+  {
+    endIndex = letter.find(' ');
+
+    if (endIndex == string::npos && letter.size() == 0)
+    {
+      break;
+    }
+    else if (endIndex == string::npos)
+    {
+      endIndex = letter.size();
+    }
+
+    string key = letter.substr(startIndex, endIndex);
+    cout << "this is Key:" << key << '\n'
+         << "this is value:" << morse[key] << '\n';
+    answer += morse[key];
+
+    letter = letter.substr(endIndex + 1 <= letter.size() ? endIndex + 1 : letter.size());
+  }
+
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  string test = ".... . .-.. .-.. ---";
+
+  cout << solution(test) << '\n';
+
+  cout << test.substr(4) << '\n';
+
+  return 0;
+}
