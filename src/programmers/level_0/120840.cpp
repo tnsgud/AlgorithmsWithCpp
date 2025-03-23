46549a9b9865bcb4f8f1aa43924b8d3d87cc7431
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-23 06:31:19 +0000

    🚩 Add the solution file for programmers No.120840

---
+++ src/programmers/level_0/120840.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int combi(int n, int r)
+{
+  if (r == 0 || n == 1 || n <= r)
+  {
+    return 1;
+  }
+
+  return combi(n - 1, r) + combi(n - 1, r - 1);
+}
+
+int solution(int balls, int share)
+{
+  int answer = combi(balls, share);
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  cout << solution(3, 2) << '\n';
+
+  return 0;
+}
