37c4eaa954c4b2bbc513ad8aeea3869ae7a43e21
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-24 11:29:52 +0000

    🚩 Add the solution file for programmers No.120844

---
+++ src/programmers/level_0/120844.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, string direction)
+{
+  vector<int> answer;
+  bool isLeft = direction == "left";
+
+  for (int i = isLeft ? 0 : numbers.size() - 1; isLeft ? i < numbers.size() : i > 0; isLeft ? i++ : i--)
+  {
+    int temp = numbers[i];
+    int index = isLeft ? i + 1 : i - 1;
+
+    numbers[i] = numbers[index];
+    numbers[index] = i;
+  }
+
+  for (auto i : numbers)
+  {
+    cout << i << ' ';
+  }
+
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
