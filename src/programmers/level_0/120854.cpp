13a1fd167dd225e841879345515f18b2fe69d2d7
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-27 11:33:38 +0000

    🚩 Add the solution file for programmers No.120854

---
+++ src/programmers/level_0/120854.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
+vector<int> solution(vector<string> strlist)
+{
+  vector<int> answer;
+
+  for (string str : strlist)
+  {
+    answer.push_back(str.length());
+  }
+
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
