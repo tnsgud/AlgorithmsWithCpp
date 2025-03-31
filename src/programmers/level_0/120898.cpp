963d7afa7ffdbe0a9ad65d5375d98196e91a2f99
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-31 13:18:30 +0000

    🚩 Add the solution file for programmers No.120898

---
+++ src/programmers/level_0/120898.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(string message) {
+  int answer = message.size() * 2;
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
