98ed318790866f2ab3d2c5ff129febae416f43e1
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 12:47:33 +0000

    🚩 Add the solution file for programmers No.120908

---
+++ src/programmers/level_0/120908.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(string str1, string str2)
+{
+  int answer = 0;
+
+  answer = str1.find(str2) != string::npos ? 1 : 2;
+
+  return answer;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  return 0;
+}
