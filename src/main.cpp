d6c824e25f9cfd4da2c53cb331200738eb801b95
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-02 12:47:38 +0000

    💊 하루한알 완료

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,10 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> quiz)
-{
-    vector<string> answer;
-    int startIndex, endIndex, num, signal, quiz_answer;
+int solution(string str1, string str2) {
+    int answer = 0;
 
-    for (string str : quiz)
-    {
-        startIndex = num = quiz_answer = 0;
-        signal = 1;
-
-        while ((endIndex = str.find(" ", startIndex)) != string::npos)
-        {
-            string s = str.substr(startIndex, endIndex - startIndex);
-
-            if (s == "+")
-            {
-                signal = 1;
-            }
-            else if (s == "-")
-            {
-                signal = -1;
-            }
-            else if (s == "=")
-            {
-                startIndex = endIndex + 1;
-                num = stoi(str.substr(startIndex, s.size() - startIndex));
-                answer.push_back(num == quiz_answer ? "O" : "X");
-            }
-            else
-            {
-                num = stoi(s);
-                quiz_answer += num * signal;
-            }
-
-            startIndex = endIndex + 1;
-        }
-    }
+    answer = str1.find(str2) != string::npos ? 1 : 2;
 
     return answer;
 }
@@ -50,11 +17,6 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
-    for (auto s : solution({"3 - 4 = -3", "5 + 6 = 11"}))
-    {
-        cout << s << '\n';
-    }
 
     return 0;
 }
