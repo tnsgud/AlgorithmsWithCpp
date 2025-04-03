133d3e79f83231f84c300621a5a1ef0dfd1284b6
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-03 13:20:27 +0000

    💊 하루한알 완료

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,8 @@
 
 using namespace std;
 
-int solution(string str1, string str2) {
+int solution(int n, int t) {
     int answer = 0;
-
-    answer = str1.find(str2) != string::npos ? 1 : 2;
 
     return answer;
 }
