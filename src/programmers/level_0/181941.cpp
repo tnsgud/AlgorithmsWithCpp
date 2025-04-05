6b3da847ebcb8617a28815423eb5e9e44202a171
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-04-05 12:44:54 +0000

    🚩 Add the solution file for programmers No.181941

---
+++ src/programmers/level_0/181941.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr)
+{
+  string answer = "";
+
+  for (string c : arr)
+  {
+    answer += c;
+  }
+
+  return answer;
+}
